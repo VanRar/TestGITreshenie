35fd964d381fcb444eab3bb0577d489a6215ff1c
VanRar <vitalikf7@gmail.com>
2020-06-27 19:37:22 +0300

    Добавьте файлы проекта.

---
+++ TestGIT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("тест гит 1. делаем мастер-ветку");
+        }
+    }
+}
